9a8ec1d60e0693ed8402a54ef58889d2a96c138e
Swastik Biswal <swastikbiswal18@gmail.com>
2026-03-13 15:32:21 +0530

    project completed

--- backend/app.js
+++ backend/app.js
@@ -1,58 +1,75 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
 
-const placesRoutes = require('./routes/places-routes');
-const usersRoutes = require('./routes/users-routes');
-const HttpError = require('./models/http-error');
+const placesRoutes = require("./routes/places-routes");
+const usersRoutes = require("./routes/users-routes");
+const HttpError = require("./models/http-error");
 
 const app = express();
 
-app.use(bodyParser.json());
+const PORT = process.env.PORT || 5000;
 
-app.use('/uploads/images', express.static(path.join('uploads', 'images')));
+app.use(express.json());
 
-app.use((req,res,next) =>{
-  res.setHeader('Access-Control-Allow-Origin', '*');
+app.use("/uploads/images", express.static(path.join("uploads", "images")));
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
   );
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
-
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
   next();
 });
 
-app.use('/api/places', placesRoutes);
-app.use('/api/users', usersRoutes);
+app.use("/api/places", placesRoutes);
+app.use("/api/users", usersRoutes);
+
+
+// Serve frontend in production
+if (process.env.NODE_ENV === "production") {
+
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
+  });
+
+}
+
 
 app.use((req, res, next) => {
-  const error = new HttpError('Could not find this route.', 404);
+  const error = new HttpError("Could not find this route.", 404);
   throw error;
 });
 
 app.use((error, req, res, next) => {
-  if(req.file) {
-    fs.unlink(req.file.path, (err) =>{
+  if (req.file) {
+    fs.unlink(req.file.path, err => {
       console.log(err);
     });
   }
+
   if (res.headerSent) {
     return next(error);
   }
+
   res.status(error.code || 500);
-  res.json({ message: error.message || 'An unknown error occurred!' });
+  res.json({ message: error.message || "An unknown error occurred!" });
 });
 
+
 mongoose
-  .connect('mongodb://localhost:27017/place-collection')
+  .connect(process.env.MONGO_URI)
   .then(() => {
-    app.listen(5000);
+    app.listen(PORT, () => {
+      console.log("Server running on port", PORT);
+    });
   })
   .catch(err => {
     console.log(err);
   });
-
